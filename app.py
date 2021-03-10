e8ec43c899c87110e3336b8e3b1fc7a1ee5fea18
HyoSeokK <37130692+HyoSeokK@users.noreply.github.com>
2021-03-10 09:37:27 +0900

    25

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route('/') 
 def hello_world(): 
-    return 'Hello World! build is done???<br>이미지 변화 테스트중입니다<br> 테스트34 <br> 지금 커밋상황 확인?<br> 커밋되면 빌드치나?' 
+    return 'Hello World! build is done???<br>이미지 변화 테스트중입니다<br> 테스트34 <br> 지금 커밋상황 확인?<br> 커밋되면 빌드치나?<br> 빌드좀 그만쳐라' 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=80)
